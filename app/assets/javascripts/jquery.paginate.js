9ae821ba7d25e70e0d789a9a39f1e7f3debc9956
vlewin <vlewin@suse.de>
2012-02-07 18:30:06 -0500

    improve jquery.paginate plugin

--- app/assets/javascripts/jquery.paginate.js
+++ app/assets/javascripts/jquery.paginate.js
@@ -3,7 +3,9 @@
   $.fn.paginate= function( options ) {  
 
     var defaults = {
-      rowsPerPage : 5
+      rowsPerPage : 10,
+      startRow : 1
+      
     };
 
     //call in the default otions
@@ -11,21 +13,38 @@
 
     return this.each(function() {
 	  var $table = $(this);
-	  $table.find('tbody tr').hide();
-	  $table.append('<div id="paginateNav"><a class="prev">Prev</a><a class="next">Next</a></div>')
+	  var $rows = $table.find('tbody tr').hide();
+	  $rows.slice(options.startRow, options.rowsPerPage+options.startRow).show();
+      
+      var navigation = '<div id="navigation" style="text-align:center; margin:0px 0px 10px 0px;">';
+      var counter = 1;
+      for(var i=0; i<$rows.size() - options.startRow; i++) {
+      	console.log(i%10)
+      	if(i%10==0 && i > 9) {
+      	  counter += 1;
+      	  navigation += '<span>&nbsp;<a href="#" class="pageLink">' + (i/10) +'</a>&nbsp;</span>';
+      	}
+      }
+      
+      if($rows.size()%10 != 0) {
+      	navigation += '<span>&nbsp;<a href="#" class="pageLink">' + counter +'</a>&nbsp;</span>';
+      }
+      
+      navigation += '</div>';
+      
+	  $table.after(navigation);
 	  
-	  $('a.next').live('click', function() {
-	  	var range = $table.find('tr').slice(1,10)
-	  	range.show();
-	  	console.log(tr)
-	  	});
-	  
-	  $('a.prev').live('click', function() {
-	    $table.find('tbody tr').hide();
-	  	var range = $table.find('tr').slice(11,20)
-	  	range.show()
-	  	console.log(tr)
-	  	});	  
+	  $('a.pageLink').live('click', function(e) {
+	    e.preventDefault();
+	    $rows.hide();
+	    var end = (parseInt($(this).html())*10);
+	    var start = options.startRow;
+	    start += end - 10;
+	     
+	    
+	    var range = $rows.slice(start, end).show();
+		return false;
+	  });	  
       // Here you add your jQuery plugin code
 
     });
